7908d6a0b5811ebeebf308a218a61098b4d2e70f
KateM3d <ekaterina8medvedeva@gmail.com>
2023-06-04 20:58:00 -0400

    add cors and connect to mongodb

--- server.js
+++ server.js
@@ -1,18 +1,42 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const app = express();
+const mongoose = require("mongoose");
 const routes = require("./Routes");
+const cors = require("cors");
+
+// const swaggerJsdoc = require("swagger-jsdoc");
+// const swaggerUi = require("swagger-ui-express");
+// const YAML = require("yamljs");
+
+require("dotenv").config();
+mongoose.set("strictQuery", false);
+
+const PORT = 4002 || process.env.port;
 
 app.use(express.json());
+app.use(cors());
 
-mongoose.connect("mongodb://localhost/flashcards", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose
+    .connect(process.env.MONGODB_LINK)
+    .then(() => {
+        console.log("Connected to MongoDb");
+    })
+    .catch((err) => console.log("Error"));
 
-app.use("/api", routes);
+app.use(routes);
 
-const PORT = 3002;
+// // Swagger setup
+// const swaggerDefinition = YAML.load("./swagger.yaml");
+//
+// const options = {
+//     swaggerDefinition,
+//     apis: ["./Routes.js"],
+// };
+//
+// const specs = swaggerJsdoc(options);
+//
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
